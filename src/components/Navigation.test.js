94224ff17427e70ff1c72f2ee657e31a2a36abc9
Caroline Pisano <pisanocaroline@gmail.com>
2019-05-29 19:35:22 -0400

    :rainbow: Setup for unit testing and small unit tests on Navigation.js

---
+++ src/components/Navigation.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {shallow} from 'enzyme'
+import Navigation from './Navigation';
+describe("Navigation", () => {
+    it('renders aside', () => {
+      const wrapper = shallow(<Navigation keys={[]}/>)
+      expect(wrapper.find('aside>nav')
+        .exists())
+        .toBe(true)
+    });
+    it('runs onChange handler from props when clicked', () => {
+        const onChange = jest.fn();
+      const wrapper = shallow(<Navigation keys={[1]} onChange={onChange}/>)
+      wrapper.find('button')
+      .simulate('click')
+      expect(onChange)
+        .toHaveBeenCalled()
+    });
+})
