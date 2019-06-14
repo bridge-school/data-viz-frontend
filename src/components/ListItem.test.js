a6e368662f38b659b1bc6d5f66ab1859df452111
qiuqili <835671347@qq.com>
2019-06-14 19:58:48 -0400

    Add test for ListItem

---
+++ src/components/ListItem.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import ListItem from './ListItem';
+import { Link } from 'react-router-dom';
+
+describe('ListItem', () => {
+  it('renders list item', () => {
+    const wrapper = shallow(<ListItem />);
+    expect(wrapper.find('li').exists()).toBe(true);
+    expect(wrapper.find(Link).exists()).toBe(true);
+  });
+});
