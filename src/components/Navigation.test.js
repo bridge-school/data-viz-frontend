96fae52101925017c89f7a5c9da41d606fc5043a
leandra <leandra.reid@gmail.com>
2019-06-15 14:05:31 -0400

    change props in navigation test

--- src/components/Navigation.test.js
+++ src/components/Navigation.test.js
@@ -3,14 +3,14 @@ import {shallow} from 'enzyme'
 import Navigation from './Navigation';
 describe("Navigation", () => {
     it('renders aside', () => {
-      const wrapper = shallow(<Navigation keys={[]}/>)
+      const wrapper = shallow(<Navigation buttons={[]}/>)
       expect(wrapper.find('aside>nav')
         .exists())
         .toBe(true)
     });
     it('runs onChange handler from props when clicked', () => {
         const onChange = jest.fn();
-      const wrapper = shallow(<Navigation keys={[1]} onChange={onChange}/>)
+      const wrapper = shallow(<Navigation buttons={[1]} onChange={onChange}/>)
       wrapper.find('button').first()
       .simulate('click')
       expect(onChange)
