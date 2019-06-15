34f05ab0b7a8c39c62f5ee4c30e41739213fba06
leandra <leandra.reid@gmail.com>
2019-06-15 13:49:47 -0400

    moving button object to store

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
 // create an object to match button name with keys
-const buttonNames = {
-  gender: 'gender identity',
-  minority: 'minority groups',
-  bootcamp: 'dev bootcamp',
-  employment_status: 'employment status'
-};
-const keys = Object.keys(buttonNames);
+// const buttonNames = {
+//   gender: 'gender identity',
+//   minority: 'minority groups',
+//   bootcamp: 'dev bootcamp',
+//   employment_status: 'employment status'
+// };
+
 
 class Navigation extends React.Component {
   handleClick = key => {
@@ -15,13 +15,15 @@ class Navigation extends React.Component {
   };
 
   render() {
+    const keys = Object.keys(this.props.buttons);
+    const { buttons } = this.props;
     return (
       <aside>
         <nav>
           {keys.map(key => (
             // when button is clicked, pass the key back to setState
             <button onClick={() => this.handleClick(key)} key={key}>
-              {buttonNames[key]}
+              {buttons[key]}
             </button>
           ))}
         </nav>
