c16aee8ba36c991910cb756b651b4222237ae6c2
qiuqili <835671347@qq.com>
2019-06-16 20:32:03 -0400

    Add test for CohortList

--- src/components/CohortList.js
+++ src/components/CohortList.js
@@ -3,34 +3,36 @@ import ListItem from './ListItem';
 import LineGraph from './LineGraph';
 import { connect } from 'react-redux';
 
-import Loader from './Loader.js'
+import Loader from './Loader.js';
 
-
-
-class CohortList extends Component {
-    render() {
+export class CohortList extends Component {
+  render() {
     return (
       <div className="wrapper bridge--cohorts">
         <h1>Cohorts</h1>
         <ul>
-
-          { this.props.loading ? <Loader/> : this.props.applicants.map(id => (
-            <ListItem key={id.label} id={id.label} />
-          )) }
+          {this.props.loading ? (
+            <Loader />
+          ) : (
+            this.props.applicants.map(id => (
+              <ListItem key={id.label} id={id.label} />
+            ))
+          )}
         </ul>
-        { this.props.loading ? '' :
-
-        <LineGraph applications={this.props.applicants}  />
-        }
+        {this.props.loading ? (
+          ''
+        ) : (
+          <LineGraph applications={this.props.applicants} />
+        )}
       </div>
     );
   }
-};
+}
 
-const mapStateToProps = (state) => ({
+const mapStateToProps = state => ({
   applicants: state.applicants.applicants.data || [],
   loading: state.applicants.loading,
   error: state.applicants.error
-})
+});
 
 export default connect(mapStateToProps)(CohortList);
