011cf80ddc592847a3e872b97d67c624bedfda3e
Rachel <835671347@qq.com>
2019-06-16 20:53:07 -0400

    [Feature][STRETCH GOALS] Add test for CohortList (#23)
    * Add test for ListItem
    
    * Add test for CohortList

--- src/components/CohortList.js
+++ src/components/CohortList.js
@@ -4,36 +4,37 @@ import LineGraph from './LineGraph';
 import { connect } from 'react-redux';
 
 import Loader from './Loader.js';
-// import formattedLabel from '../helpers/removeDashCohort';
 
 
-
-class CohortList extends Component {
-    render() {
-
+export class CohortList extends Component {
+  render() {
 
     return (
       <div className="wrapper bridge--cohorts">
         <h1>Cohorts</h1>
         <ul>
-
-          { this.props.loading ? <Loader/> : this.props.applicants.map(id => (
-            <ListItem key={id.label} id={id.label} />
-          )) }
+          {this.props.loading ? (
+            <Loader />
+          ) : (
+            this.props.applicants.map(id => (
+              <ListItem key={id.label} id={id.label} />
+            ))
+          )}
         </ul>
-        { this.props.loading ? '' :
-
-        <LineGraph applications={this.props.applicants}  />
-        }
+        {this.props.loading ? (
+          ''
+        ) : (
+          <LineGraph applications={this.props.applicants} />
+        )}
       </div>
     );
   }
-};
+}
 
-const mapStateToProps = (state) => ({
+const mapStateToProps = state => ({
   applicants: state.applicants.applicants.data || [],
   loading: state.applicants.loading,
   error: state.applicants.error
-})
+});
 
 export default connect(mapStateToProps)(CohortList);
