6a6e5bceba300b12c3185c64d80fd4fccc7ce0ce
Caroline Pisano <pisanocaroline@gmail.com>
2019-06-17 19:56:47 -0400

    Halleloo I finally got this test working without errors

--- src/components/CohortDetail.test.js
+++ src/components/CohortDetail.test.js
@@ -3,26 +3,25 @@ import { shallow } from 'enzyme';
 import { CohortDetail } from './CohortDetail.js';
 import Navigation from './Navigation.js';
 import BarGraph from './BarGraph.js';
-import { fetchCohort } from '../store/actions';
 
 describe('Cohort Detail', () => {
-    const cohortDetails = {
-      bootcamp: {
-        label: 'i have not attended a bootcamp',
-        value: 19
-      }
-    };
-    let params = 'cohort-6';
+    // Setup
+    const cohortDetails = {'gender':[
+        {label: "woman", value: 39}, 
+        {label: "non-binary", value: 2}, 
+        {label: "man", value: 2}
+    ]};
+    const props = jest.fn();
+    const currentButton = 'gender'
+    const match = {params : {id: 'cohort-6'}};
+    const onChange = jest.fn();
   it('renders div', () => {
-    const wrapper = shallow(<CohortDetail id={params} />);
+    const wrapper = shallow(<CohortDetail match={match} cohortDetails={cohortDetails[currentButton]} buttons={currentButton} fetchCohort={props} />);
     expect(wrapper.find('div')
         .exists())
-        .toBe(true)
-        
-    // expect(wrapper.find(ListItem)).toHaveLength(cohortDetails.length);
+        .toBe(true) 
 });
   it('runs change handler when clicked', () => {
-    const onChange = jest.fn();
     const wrapper = shallow(
         <Navigation buttons={[1]} onChange={onChange} />
     );
